2c6ccdf0745cfacf078a3f8b3cd55cf749989f1a
Robert Apikyan <robert.apikyan@volo.global>
2023-05-18 23:34:59 +0400

    added unit test

--- test/api_tests.dart
+++ test/api_tests.dart
@@ -1,11 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_rest/http_rest.dart';
 
-void main() {
-
-  // final client =
-  //     HttpRestClient.builder(DefaultRequestExecutor(http.Client())).build();
-  //
-  // client.execute(HttpRestRequest(
-  //     method: Methods.post,
-  //     url: '',));
+void main() async {
+  test_get_profile();
 }
+
+UrlBuilder baseUrl() => UrlBuilder.base(
+    'https://my-json-server.typicode.com/RobertApikyan/JsonData/');
+
+HttpRestRequest profile() => HttpRestRequest(
+      method: Methods.post,
+      url: baseUrl().url('profile').toString(),
+    );
+
+void test_get_profile() => test('test_make_calls_without_converter', () async {
+      final client =
+          HttpRestClient.builder(DefaultRequestExecutor(http.Client()))
+              .addResponseConverter(JsonToMapResponseConverter())
+              .addRequestConverter(MapToJsonRequestConverter())
+              .build();
+
+      HttpRestRequest profile() => HttpRestRequest(
+          method: Methods.post,
+          responseConverterType: JsonToMapResponseConverter,
+          url: UrlBuilder.base(
+                  'https://my-json-server.typicode.com/RobertApikyan/JsonData/')
+              .url('profile')
+              .toString());
+
+      final result = await client.execute(profile());
+
+      expect(result.rowResponse.code, 201);
+      expect(result.response is Map<String, dynamic>, true);
+      if (result.response
+          case {"id": int id, "name": String name, "skills": [String a]}) ;
+    });
