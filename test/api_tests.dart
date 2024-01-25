b97d9eb4c567b91755188437bd34f35ed594c8d3
Robert Apikyan <robert.apikyan@volo.global>
2024-01-25 21:09:04 +0400

    - Added test for request read progress tracking - version raised to 1.0,0

--- test/api_tests.dart
+++ test/api_tests.dart
@@ -1,6 +1,5 @@
 import 'dart:typed_data';
-
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_rest/http_rest.dart';
 
@@ -9,6 +8,7 @@ void main() async {
     test_get_profile();
     test_post_create_profile();
     test_post_row_create();
+    test_get_download_progress();
   });
 }
 
@@ -85,6 +85,8 @@ void test_post_row_create() =>
 
       HttpRestRequest profile() => HttpRestRequest(
           method: Methods.post,
+          writeProgressListener: (bytes, totalBytes) => print('upload progress= ${bytes/totalBytes}'),
+          readProgressListener: (bytes, totalBytes) => print('download progress= ${bytes/totalBytes}'),
           url: UrlBuilder.base(
                   'https://my-json-server.typicode.com/RobertApikyan/JsonData/')
               .url('profile')
@@ -95,5 +97,30 @@ void test_post_row_create() =>
       final result = await client.execute(profile());
 
       expect(result.rowResponse.code, 201);
+      expect(result.response is Uint8List, true);
+    });
+
+
+void test_get_download_progress() =>
+    test('test_get_download_progress', () async {
+      final client =
+          HttpRestClient.builder(DefaultRequestExecutor(http.Client()))
+              .addResponseConverter(JsonToMapResponseConverter())
+              .addRequestConverter(MapToJsonRequestConverter())
+              .build();
+
+      HttpRestRequest introImage() => HttpRestRequest(
+          method: Methods.get,
+          writeProgressListener: (bytes, totalBytes) => print('upload progress= ${bytes/totalBytes}'),
+          readProgressListener: (bytes, totalBytes) => print('download progress= ${bytes/totalBytes}'),
+          url: UrlBuilder.base(
+                  'https://raw.githubusercontent.com/RobertApikyan/http_rest/')
+              .url('main/doc/assets/{image}')
+              .addPath('image','intro.png')
+              .toString());
+
+      final result = await client.execute(introImage());
+
+      expect(result.rowResponse.code, 200);
       expect(result.response is Uint8List, true);
     });
